9609fcae7eddd87c2fab980768f0031bdc95ec10
phanick <39258517+phanick@users.noreply.github.com>
2022-07-29 17:34:49 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, this is python app1 running in kubernetes!"
+    return "python-app1"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
