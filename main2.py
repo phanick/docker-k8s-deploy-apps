a4e83700060cfa4e2572c863fd1f6f5b6ac78992
Phani Kumar Cherughattu <phanikumar.cherughattu@external.stellantis.com>
2022-07-29 17:13:08 +0530

    added

--- main2.py
+++ main2.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "python-new- app2"
+    return "python-app2-content added."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
